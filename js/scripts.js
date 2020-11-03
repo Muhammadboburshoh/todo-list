d5a68abdd08448da4009c6ac514e10cbc43c45d1
Muhammadboburshoh <muhammadbobur600@gmail.com>
2020-11-03 20:36:18 +0500

    Finish todo list

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+var elTodoForum = document.querySelector(".todo-list");
+var elTodoInput = elTodoForum.querySelector(".todo-list__input");
+var elTodoCheckbox = elTodoForum.querySelector(".todo-list__important-work");
+var elTodoList = document.querySelector(".formed__list");
+
+var todoList = [];
+
+elTodoForum.addEventListener("submit", function(evt) {
+  evt.preventDefault();
+
+  var todoInput = elTodoInput.value.trim();
+
+  if(todoInput === "") {
+    alert("Biror bir ish kiriting brat:)");
+    return;
+  }
+
+  if(elTodoCheckbox.checked) {
+    todoList.unshift(todoInput);
+  }
+  else {
+    todoList.push(todoInput);
+  }
+
+  elTodoList.innerHTML = '';
+
+  for(var todo of todoList) {
+    var todoListItem = document.createElement("LI");
+    elTodoList.appendChild(todoListItem);
+    todoListItem.classList.add("list-group-item", "bg-success")
+    todoListItem.textContent = todo;
+    console.log(todoList);
+  }
+
+
+  elTodoInput.value = "";
+  elTodoInput.focus();
+
+  // console.log(todoList);
+})
+
